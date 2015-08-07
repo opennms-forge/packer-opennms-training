767aa09bf2e66b40579d2b7d7a171403bb398fe3
Ronny Trommer <ronny@opennms.org>
2015-08-07 12:22:07 +0200

    Renamed to template.json and changed to minimal ISO

--- centos-7.1/scripts/virtualbox.sh
+++ centos-7.1/scripts/virtualbox.sh
@@ -6,6 +6,5 @@ yum install -y bzip2
 cd /tmp
 mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
-/opt/VBoxGuestAdditions-$VBOX_VERSION/init/vboxadd setup
 umount /mnt
 rm -rf /home/vagrant/VBoxGuestAdditions_*.iso
