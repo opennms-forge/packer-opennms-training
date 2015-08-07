3003eba8226df954a12342d1e33733511ee9dcca
Ronny Trommer <ronny@opennms.org>
2015-08-07 09:01:16 +0200

    Set to latest Horizon

--- centos-7.1/scripts/opennms-prepare.sh
+++ centos-7.1/scripts/opennms-prepare.sh
@@ -2,7 +2,7 @@
 yum -y install net-tools wget htop iftop sysstat tcpdump vim git tar unzip
 
 # Install OpenNMS repository
-rpm -Uvh http://yum.opennms.org/repofiles/opennms-repo-snapshot-rhel7.noarch.rpm
+rpm -Uvh http://yum.opennms.org/repofiles/opennms-repo-stable-rhel7.noarch.rpm
 rpm --import http://yum.opennms.org/OPENNMS-GPG-KEY
 
 # Install OpenNMS without any setup
