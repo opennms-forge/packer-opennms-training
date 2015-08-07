1f2b005b8a225b0826fc2dcc268d1913b176e818
Ronny Trommer <ronny@opennms.org>
2015-08-07 10:34:50 +0200

    Add vboxadd setup command

--- centos-7.1/scripts/virtualbox.sh
+++ centos-7.1/scripts/virtualbox.sh
@@ -1,11 +1,11 @@
 VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 
-# required for VirtualBox 4.3.26
+# required for VirtualBox Guest Additions
 yum install -y bzip2
 
 cd /tmp
 mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
+/opt/VBoxGuestAdditions-$VBOX_VERSION/init/vboxadd setup
 umount /mnt
 rm -rf /home/vagrant/VBoxGuestAdditions_*.iso
-
