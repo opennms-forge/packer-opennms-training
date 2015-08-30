66df201d9a505e49bac859283bb4812df9a2d40c
Ronny Trommer <ronny@opennms.org>
2015-08-30 21:48:36 +0200

    Added additional tools for mail, dns, net-snmp and rrdtool

--- centos-7.1/scripts/opennms-prepare.sh
+++ centos-7.1/scripts/opennms-prepare.sh
@@ -1,5 +1,10 @@
 # Install some tools
-yum -y install net-tools wget htop iftop sysstat tcpdump vim git tar unzip
+yum -y install net-tools wget htop iftop sysstat tcpdump vim git tar unzip psmisc
+
+# Additional packages
+yum -y install rrdtool jrrd jrrd2
+yum -y install net-snmp-utils net-snmp
+yum -y install bind-utils mailx
 
 # Install OpenNMS repository
 rpm -Uvh http://yum.opennms.org/repofiles/opennms-repo-stable-rhel7.noarch.rpm
